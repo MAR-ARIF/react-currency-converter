f5d28fe3c56437f0d21492ef14218975d0aae01d
Arif <arifmarahman19@gmail.com>
2026-04-23 21:31:13 +0100

    Add tailwind css styling for input box component

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -16,13 +16,14 @@ function InputBox({
 
     const amountInputId = useId();
     return(
-        <div className="bg-blue-100 flex">
-            <div className="w-1/2 text-black/40">
-                <label htmlFor={amountInputId} >
+        <div className="bg-white p-3 rounded-lg flex">
+            <div className="w-1/2">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block" >
                     {label}
                 </label>
                 <input 
                     id={amountInputId}
+                    className="bg-transparent py-1.5 w-full outline-none"
                     type="number"
                     placeholder="amount"
                     disabled={amountDisabled}
@@ -30,10 +31,10 @@ function InputBox({
                     onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
-            <div className="w-1/2 text-black/40">
-                <p >Currency Type</p>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
-                 className="cursor-pointer"
+                 className="cursor-pointer bg-gray-100 px-1 py-1 rounded-lg outline-none"
                  value={selectCurrency}
                  disabled={currencyDisabled}
                  onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
