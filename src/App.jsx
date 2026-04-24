64484f50a6effe4d3c613c8c66cd2b4c2cf227ac
Arif <arifmarahman19@gmail.com>
2026-04-24 11:58:59 +0100

    Tailwind styling added for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,16 +23,16 @@ function App() {
   
 
   return (
-    <div className="w-full h-screen bg-cover bg-no-repeat"
+    <div className="w-full h-screen bg-cover bg-no-repeat flex justify-center items-center"
     style={{backgroundImage: `url(${backgroundImage})`}}>
-      <div>
+      <div className="w-full max-w-md mx-auto bg-white/30 backdrop-blur-sm rounded-lg p-5 border border-white/20">
         <form 
         onSubmit={(e) => {
           e.preventDefault();
           convert();
           
         }}>
-          <div>
+          <div className="w-full mb-1">
             <InputBox
               label="From"
               amount={amount}
@@ -42,14 +42,19 @@ function App() {
               onAmountChange={setAmount}
             />
           </div>
-          <div>
+          <div className="relative w-full h-0.5">
             <button
+            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 
+            bg-blue-500 text-white px-4 py-1 rounded-md border-2 border-white
+            transition duration-200
+            hover:bg-blue-600
+            active:scale-95"
             type="button"
             onClick={swap}>
               swap
             </button>
           </div>
-          <div>
+          <div className="w-full mb-4 mt-1">
             <InputBox
               label="To"
               amountDisabled
@@ -60,6 +65,7 @@ function App() {
             />
           </div>
           <button
+          className="w-full text-white bg-blue-500 py-3 px-4 rounded-lg"
           type="submit">
             Convert {from.toUpperCase()} to {to.toUpperCase()}
           </button>
